99c6b69e52590a29166b2a933038cbc43e65f368
0123sandeepkumar <speedan0123@gmail.com>
2023-07-30 11:33:20 +0530

    final

---
+++ src/config.js
@@ -0,0 +1,8 @@
+// let BASE_URL = "https://post-it-heroku.herokuapp.com/";
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+//   BASE_URL = "http://localhost:4000/";
+// }
+//let BASE_URL = "https://socialmediaappserver-e2ws.onrender.com/";
+//let BASE_URL = "http://localhost:4000/";
+let BASE_URL= "https://thoughtshare-backend.onrender.com/";
+export { BASE_URL };
